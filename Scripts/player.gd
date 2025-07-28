50a46dd8d925f83fc9cdce269b841554eb8414b8
Jeremias Wahlsten <jere.wahlsten@gmail.com>
2025-07-28 14:10:31 +0300

    finished player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,6 @@
 class_name Player extends CharacterBody2D
 
-@onready var animationPlayer = $AnimationPlayer
-@onready var sprite = $playerTexture
+@onready var sprite = $AnimatedSprite2D
 @onready var audio = $Audio
 
 @export var speed = 300.0
@@ -10,12 +9,14 @@ class_name Player extends CharacterBody2D
 @export var acceleration = 600
 @export var deceleration = 800
 
+var isHurt = false
+
 
 func _physics_process(delta: float) -> void:
 	handleInput(delta)
+	handle_animation(delta)
 	
-func handleInput(delta):
-
+func handleInput(delta: float):
 	# Handle horizontal movement
 	if Input.is_action_pressed("left"):
 		velocity.x = move_toward(velocity.x, -speed, acceleration * delta)
@@ -25,8 +26,23 @@ func handleInput(delta):
 		# Decelerate when no input
 		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
 
-	# Handle Jump.
+	# Handle Jump
 	if Input.is_action_just_pressed("up") and is_on_floor():
-		animationPlayer.play("jump")
+		sprite.play("jump")
 		audio.playJump()
 		velocity.y = jump_speed
+		
+		
+func handle_animation(delta: float):
+	# Handle animations except jump
+	if isHurt:
+		sprite.play("hurt")
+	elif !is_on_floor():
+		sprite.play("jump")
+	
+	elif velocity.x != 0:
+		if velocity.x < 0:
+			sprite.flip_h = true
+		sprite.play("walk")
+	else:
+		sprite.play("idle")
